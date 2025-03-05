5cc487729db4d97c9cd89269306003c340b3e51f
Abdullag4 <abdullakawkas4@gmail.com>
2025-03-05 09:18:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def main():
 
     if page == "Home":
         home.home()
-    elif page == "Add Item":
+    elif page == "Item":
         mainitem.item_page()
     elif page == "Receive Items":
         receive_items.receive_items()  # ✅ Call the new function
