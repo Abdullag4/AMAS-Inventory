3efb4d1922b362ddf6358d39a65bcd832cbd6bd9
Abdullag4 <abdullakawkas4@gmail.com>
2025-03-08 13:36:50 +0300

    Update trackpo.py

--- PO/trackpo.py
+++ PO/trackpo.py
@@ -1,1 +1,13 @@
+import streamlit as st
+from PO.po_handler import POHandler
 
+po_handler = POHandler()
+
+def track_po_tab():
+    st.header("🚚 Track Purchase Orders")
+    po_details = po_handler.get_all_purchase_orders()
+
+    if not po_details.empty:
+        st.dataframe(po_details, use_container_width=True)
+    else:
+        st.info("No purchase orders to display.")
