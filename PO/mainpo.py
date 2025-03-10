2baa8119cd9587550a84faa988216c7e156f1a1f
Abdullag4 <abdullakawkas4@gmail.com>
2025-03-10 21:48:31 +0300

    Update mainpo.py

--- PO/mainpo.py
+++ PO/mainpo.py
@@ -1,10 +1,11 @@
 import streamlit as st
 from PO.manualpo import manual_po_tab
+from PO.trackpo import track_po_tab
 
 def po_page():
     st.title("🛒 Purchase Order Management")
 
-    tabs = st.tabs(["Manual PO"])  # ✅ Only Manual PO active now for clarity
+    tabs = st.tabs(["Manual PO", "Tracking PO"])  # ✅ Only Manual PO active now for clarity
 
     with tabs[0]:
         manual_po_tab()
