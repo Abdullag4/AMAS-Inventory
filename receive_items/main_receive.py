119ae444286e3719d7849b257b94e3890c3ad390
Abdullag4 <abdullakawkas4@gmail.com>
2025-03-11 22:07:54 +0300

    Update main_receive.py

--- receive_items/main_receive.py
+++ receive_items/main_receive.py
@@ -1,1 +1,14 @@
+import streamlit as st
+from receive_items.receive_items import receive_items
+from receive_items.received_po import received_po_tab
 
+def main_receive_page():
+    st.title("📦 Receive Items Management")
+
+    tabs = st.tabs(["Manual Receive", "Received PO"])
+
+    with tabs[0]:
+        receive_items()
+
+    with tabs[1]:
+        received_po_tab()
