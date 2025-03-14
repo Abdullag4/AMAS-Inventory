c6a7f32201dd906211b4c631a93b72999415f106
Abdullag4 <abdullakawkas4@gmail.com>
2025-03-14 11:14:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,23 +2,26 @@ import streamlit as st
 import home
 from item import mainitem
 import PO.mainpo as mainpo
-from receive_items.main_receive import main_receive_page  # ✅ Updated import
+from receive_items import main_receive
+import reports.main_reports as main_reports
 
 st.set_page_config(page_title="Inventory Management System", layout="wide")
 
 def main():
     """Main entry point for the Inventory Management app."""
     st.sidebar.title("Navigation")
-    page = st.sidebar.radio("Go to", ["Home", "Item", "Receive Items", "Purchase Order"])  # ✅ Added "Purchase Order"
+    page = st.sidebar.radio("Go to", ["Home", "Item", "Receive Items", "Purchase Order", "Reports"])
 
     if page == "Home":
         home.home()
     elif page == "Item":
         mainitem.item_page()
     elif page == "Receive Items":
-        main_receive_page()  # ✅ Updated to use main_receive_page
+        main_receive.main_receive_page()
     elif page == "Purchase Order":
         mainpo.po_page()
+    elif page == "Reports":
+        main_reports.main_reports()
 
 if __name__ == "__main__":
     main()
