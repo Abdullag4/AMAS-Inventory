d70c0eeac3a2f1e46c8fc5b267ef7f55a9c068dd
Abdullag4 <abdullakawkas4@gmail.com>
2025-03-17 21:06:39 +0300

    Update main_receive.py

--- receive_items/main_receive.py
+++ receive_items/main_receive.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from receive_items.receive_items import receive_items
 from receive_items.received_po import received_po_tab
-from receive_items.item_location import item_location
+from receive_items.item_location import item_location_tab
 
 def main_receive_page():
     st.title("📦 Receive Items Management")
